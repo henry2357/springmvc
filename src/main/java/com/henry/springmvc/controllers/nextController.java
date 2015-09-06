57c320399992ca966348e8c58c578667f8195b4c
ewwuxin <442913509@qq.com>
2015-09-06 17:03:37 +0800

    add log implementation slf4j & logback

--- src/main/java/com/henry/springmvc/controllers/nextController.java
+++ src/main/java/com/henry/springmvc/controllers/nextController.java
@@ -10,16 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpSession;
 
 @Controller
-public class DesktopProfileController {
-	@RequestMapping(value = "index.do", method = RequestMethod.GET)
+public class nextController {
+	Logger logger = LoggerFactory.getLogger(nextController.class);
+
+	@RequestMapping(value = "next.do", method = RequestMethod.GET)
 	public String render(Model model,
 						 HttpSession session) {
-		Logger logger = LoggerFactory.getLogger("DesktopProfileController:render()");
-
-		logger.info(session.getId());
-		session.setAttribute("attr1", "value1");
-		model.addAttribute("attr1", "attr1-value");
-		model.addAttribute("url", "next.do");
-		return "index";
+		logger.warn("nextController:render()");
+		String value = (String)session.getAttribute("attr1");
+		model.addAttribute("valueTest", value);
+		return "next";
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/next.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head>
+<body>
+<p>
+    <span>${valueTest}</span>
+</p>
+</body>
+</html>
